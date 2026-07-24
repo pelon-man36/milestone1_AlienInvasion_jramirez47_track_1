d199d9ffb846d06f41532fb7db05229022d39700
Johan Ramirez <johand200505@gmail.com>
2026-07-24 14:40:40 -0400

    M1 (IMPORTANT) - Changed events for ship, moving up/down, built-in boundaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -49,16 +49,16 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
--- ship.py
+++ ship.py
@@ -19,19 +19,19 @@ class Ship:
 
         self.rect = self.image.get_rect()
         self.rect.midleft = self.boundaries.midleft
-        self.moving_right = False
-        self.moving_left = False
-        self.x = float(self.rect.x)
+        self.moving_up = False
+        self.moving_down = False
+        self.y = float(self.rect.y)
 
     def update(self):
         # Updating the position of ship
         temp_speed = self.settings.ship_speed
-        if self.moving_right and self.rect.right < self.boundaries.right:
-            self.x += temp_speed
-        if self.moving_left and self.rect.left > self.boundaries.left:
-            self.x -= temp_speed
+        if self.moving_up and self.rect.top > self.boundaries.top:
+            self.y -= temp_speed
+        if self.moving_down and self.rect.bottom < self.boundaries.bottom:
+            self.y += temp_speed
 
-        self.rect.x = self.x
+        self.rect.y = self.y
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
