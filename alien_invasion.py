e564534d49df4f1ccc9c7f77387a5d1401ff6515
Johan Ramirez <johand200505@gmail.com>
2026-07-31 15:17:08 -0400

    M2 - Alien fleet resets when completely destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,6 +72,10 @@ class AlienInvasion:
             self.impact_sound.play()
             self.impact_sound.fadeout(500)
 
+        if self.alien_fleet.check_destroyed_status():
+            self._reset_level()
+
+
     def _reset_level(self):
         self.ship.arsenal.arsenal.empty()
         self.alien_fleet.fleet.empty()
