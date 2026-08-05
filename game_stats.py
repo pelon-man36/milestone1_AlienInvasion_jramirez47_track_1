0e49a62c6716601f0393eca429ad3d8dbc8314d3
Johan Ramirez <johand200505@gmail.com>
2026-08-05 15:05:56 -0400

    M3 - Fleshed out game stats to update the hi score and saved and read from a file

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,5 @@
+# from pathlib import Path
+import json
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
@@ -10,8 +12,29 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
 
         self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get("hi_score", 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+
+    def save_scores(self):
+        scores = {
+            "hi_score": self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f"File Not Found: {e}")
 
     def reset_stats(self):
         self.ships_left = self.settings.starting_ship_count
@@ -21,12 +44,18 @@ class GameStats():
     def update(self, collisions):
         self._update_score(collisions)
         self._update_max_score()
+        self._update_hi_score()
 
 
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
         # print(f"Max: {self.max_score}")
+
+    def _update_hi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+        # print(f"Hi: {self.hi_score}")
 
     def _update_score(self, collisions):
         for alien in collisions.values():
