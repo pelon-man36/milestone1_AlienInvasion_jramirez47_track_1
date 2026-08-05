f28e29f314c59169dc6faf61d9660acb9e43031f
Johan Ramirez <johand200505@gmail.com>
2026-08-05 14:43:28 -0400

    M3 - Fleshed out game stats to be updated by the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self.settings.starting_ship_count)
+        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w,self.settings.screen_h)
@@ -81,11 +81,13 @@ class AlienInvasion:
         if collisons:
             self.impact_sound.play()
             self.impact_sound.fadeout(500)
+            self.game_stats.update(collisons)
+
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
             self.settings.increase_difficulty()
-            # update game stats
+            self.game_stats.update_level()
 
             # update hud view
 
@@ -109,7 +111,7 @@ class AlienInvasion:
 
     def restart_game(self):
         self.settings.initialize_dynamic_settings()
-        # reset game stats
+        self.game_stats.reset_stats()
 
         # update HUD scores
 
--- game_stats.py
+++ game_stats.py
@@ -1,6 +1,38 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
 class GameStats():
     """Stores everthing needed for game stats."""
 
-    def __init__(self, ships_left):
+    def __init__(self, game: "AlienInvasion"):
         """Initial setup. Currently only holds lives."""
-        self.ships_left = ships_left
+        self.game = game
+        self.settings = game.settings
+        self.max_score = 0
+
+        self.reset_stats()
+
+    def reset_stats(self):
+        self.ships_left = self.settings.starting_ship_count
+        self.score = 0
+        self.level = 1
+
+    def update(self, collisions):
+        self._update_score(collisions)
+        self._update_max_score()
+
+
+    def _update_max_score(self):
+        if self.score > self.max_score:
+            self.max_score = self.score
+        # print(f"Max: {self.max_score}")
+
+    def _update_score(self, collisions):
+        for alien in collisions.values():
+            self.score += self.settings.alien_points
+        # print(f"Basic: {self.score}")
+
+    def update_level(self):
+        self.level += 1
+        print(f"Level: {self.level}")
